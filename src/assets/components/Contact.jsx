e17bafa224f7cdb145026cc345fb2838858e43e9
UmerUchiha10 <umerfarooq14.aa27@gmail.com>
2024-05-08 00:02:51 +0500

    updated buttons

--- src/assets/components/Contact.jsx
+++ src/assets/components/Contact.jsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 const Contact = () => {
 return (
 <div>
-    <Navbar/>
+    <Navbar />
     <section class=" bg-red-900">
         <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-white ">Contact Us
@@ -18,14 +18,13 @@ return (
                         email</label>
                     <input type="email" id="email"
                         class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                        placeholder="Enter Name" required/>
+                        placeholder="Enter Name" required />
                 </div>
                 <div>
-                    <label for="subject"
-                        class="block mb-2 text-sm font-medium text-white">Subject</label>
+                    <label for="subject" class="block mb-2 text-sm font-medium text-white">Subject</label>
                     <input type="text" id="subject"
                         class="block p-3 w-full text-sm  bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500  dark:border-gray-600 dark:text-black  dark:focus:ring-primary-500  dark:shadow-sm-light"
-                        placeholder="Let us know how we can help you" required/>
+                        placeholder="Let us know how we can help you" required />
                 </div>
                 <div class="sm:col-span-2">
                     <label for="message" class="block mb-2 text-sm font-medium text-white">Your
@@ -34,9 +33,15 @@ return (
                         class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black  dark:focus:ring-primary-500 dark:focus:border-primary-500"
                         placeholder="Leave a comment..."></textarea>
                 </div>
-                <button type="submit"
-                    class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-red-600 hover:bg-red-950 ">Send
-                    message</button>
+
+                <button
+                    class="cursor-pointer transition-all 
+bg-gray-700 text-white px-6 py-2 rounded-lg
+border-yellow-400
+border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
+active:border-b-[2px] active:brightness-90 active:translate-y-[2px] hover:shadow-xl hover:shadow-green-300 shadow-green-300 active:shadow-none">
+                    Send Message
+                </button>
             </form>
         </div>
     </section>
--- src/assets/components/Todolist.jsx
+++ src/assets/components/Todolist.jsx
@@ -2,22 +2,34 @@ import React from 'react'
 import { CiEdit } from "react-icons/ci";
 import { MdDelete } from "react-icons/md";
 
-const Todolist = ({todo, todos, handleAdd,handleCheckbox,handleChange,handleEdit,handleDelete,showfinished,togglefinished}) => {
+const Todolist = ({todo, todos,
+handleAdd,handleCheckbox,handleChange,handleEdit,handleDelete,showfinished,togglefinished}) => {
 return (
 <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-red-100 min-h-[80vh] md:w-[35%]">
     <h1 className='text-center font-bold text-3xl'>Tasker! Complete Instantly</h1>
     <div className="addtodo my-5 flex flex-col gap-4">
         <h2 className="text-2xl font-bold">Add Todo</h2>
         <div className="flex">
-        <input type="text" className="w-full rounded-full p-2" onChange={handleChange} value={todo} />
-        <button onClick={handleAdd} disabled={todo.length<3}
-            className="bg-red-800 disabled:bg-red-700 hover:bg-red-950 p-4 py-2 text-sm font-bold text-white rounded-full mx-2">
-            Add
-        </button>
+            <input type="text" className="w-full rounded-full p-2" onChange={handleChange} value={todo} />
+            
+
+            <button onClick={handleAdd} disabled={todo.length<3}
+                className="overflow-hidden  w-32 p-2 mx-2 h-12 bg-black text-white border-none rounded-lg text-xl font-bold cursor-pointer relative z-10 group">
+                Add!
+                <span
+                    className="absolute w-36 h-32 -top-8 -left-2 bg-red-200 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-right"></span>
+                <span
+                    className="absolute w-36 h-32 -top-8 -left-2 bg-red-400 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-right"></span>
+                <span
+                    className="absolute w-36 h-32 -top-8 -left-2 bg-red-600 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-right"></span>
+                <span
+                    className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10">Save!</span>
+            </button>
+
         </div>
-       
+
     </div>
-    <input type="checkbox" onChange={togglefinished} checked={showfinished} className='my-4 mx-2'/> Show Finished
+    <input type="checkbox" onChange={togglefinished} checked={showfinished} className='my-4 mx-2' /> Show Finished
     <div className='h-[1px] bg-black opacity-15 w-[90%] mx-auto my-2'>
 
     </div>
@@ -36,17 +48,30 @@ return (
                 </div>
             </div>
 
-            <div className="buttons flex h-full">
+            <div className="buttons flex h-full gap-2">
+
+
                 <button onClick={(e)=> handleEdit(e,item.id)}
-                    className="bg-red-800 hover:bg-red-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
-                    >
-                    <CiEdit />
+                    className="bg-red-950 text-red-400 border border-red-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+                    <span
+                        className="bg-red-400 shadow-red-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+                    Edit
                 </button>
+
                 <button onClick={(e)=> handleDelete(e, item.id)}
-                    className="bg-red-800 hover:bg-red-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
-                    >
-               <MdDelete />
+                    className="inline-flex items-center px-4 py-2 bg-red-600 transition ease-in-out delay-75 hover:bg-red-700 text-white text-sm font-medium rounded-md hover:-translate-y-1 hover:scale-110">
+                    <svg stroke="currentColor" viewBox="0 0 24 24" fill="none" class="h-5 w-5 mr-2"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                            stroke-width="2" stroke-linejoin="round" stroke-linecap="round"></path>
+                    </svg>
+
+                    Delete
                 </button>
+
+
+
             </div>
         </div>
         ))}
