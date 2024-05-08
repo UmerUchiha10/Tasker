07b6a517ab9a80dbcd0118a1c9296c85ec2ebbb9
UmerUchiha10 <umerfarooq14.aa27@gmail.com>
2024-05-08 13:48:51 +0500

    change design of about

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -3,34 +3,57 @@ import logo from "../../images/list.png"
 import Navbar from './Navbar';
 
 const About = () => {
-  return (
-  <> 
-  <Navbar/>
-    <div className="flex justify-center items-center h-screen  py-4">
-      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2 w-full md:w-1/2">
-        <div className="-mx-3 md:flex mb-6">
-          <div className="md:w-full px-3">
-            <div className="flex justify-center">
-            <img src={logo} alt="Phone Icon" style={{ width: '45px', height: '45px' }} />
-            </div>
-            <h2 className="block uppercase tracking-wide text-grey-darker text-xl text-center font-bold mb-2">Tasker! Complete Instantly</h2>
-            <p className="block text-grey-darker text-base leading-relaxed mb-6">
-              Introducing Tasker! Complete Instantly, the ultimate to-do list application meticulously crafted with the power of React.js components. Tasker is not just another task manager; it's a seamless blend of efficiency, functionality, and user-centric design, meticulously engineered to elevate your productivity to new heights.
-            </p>
-          
-            <ul className="list-disc pl-5 mb-6">
-              <li>Add Tasks: Seamlessly add tasks to your to-do list with just a few clicks or taps. Whether it's a work assignment, a personal errand, or a project milestone.</li>
-              <li>Update and Delete Tasks: Need to make changes to your tasks? Tasker offers intuitive options to update or delete tasks with ease.</li>
-              <li>Check off Completed Tasks: Celebrate your accomplishments by checking off tasks as you complete them. Tasker keeps track of your progress.</li>
-              <li>Show and Hide Completed Tasks: Maintain a clutter-free workspace by effortlessly toggling between displaying and hiding completed tasks. </li>
-            </ul>
-           
-          </div>
+return (
+<>
+    <Navbar />
+
+
+    <div className='flex justify-center my-3'>
+
+    <div class="relative flex flex-col md:flex-row bg-clip-border rounded-xl bg-white text-gray-700 shadow-md w-full max-w-[48rem]">
+    
+    <div class="p-6">
+        <div className='flex justify-center gap-2'>
+        <img src={logo} alt="Phone Icon" style={{ width: '35px', height: '35px' }} />
+        <h6 class="block mb-4 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-gray-700 uppercase">
+            Tasker! Complete Instantly
+        </h6>
         </div>
-      </div>
+       
+        <h4 class="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+            Introducing Tasker! Complete Instantly, the ultimate to-do list application meticulously crafted
+            with the power of React.js components
+        </h4>
+        <ul class="block mb-8 font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
+            <li>add tasks to your to-do list.it Has options to update or delete tasks with ease.asker keeps track
+                of your progress. toggling between displaying and hiding completed tasks.</li>
+        </ul>
+        <a href="#" class="inline-block">
+            <div class="w-full h-40 flex items-center justify-center cursor-pointer">
+                <div class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold shadow text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 dark:bg-gray-700 dark:text-white dark:hover:text-gray-200 dark:shadow-none group">
+                    <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-red-600 group-hover:h-full"></span>
+                    <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" class="w-5 h-5 text-green-400">
+                            <path d="M14 5l7 7m0 0l-7 7m7-7H3" stroke-width="2" stroke-linejoin="round" stroke-linecap="round"></path>
+                        </svg>
+                    </span>
+                    <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" class="w-5 h-5 text-green-400">
+                            <path d="M14 5l7 7m0 0l-7 7m7-7H3" stroke-width="2" stroke-linejoin="round" stroke-linecap="round"></path>
+                        </svg>
+                    </span>
+                    <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white dark:group-hover:text-gray-200">Learn More</span>
+                </div>
+            </div>
+        </a>
     </div>
-    </>  
-  );
+</div>
+
+    </div>
+
+
+</>
+);
 };
 
 export default About;
